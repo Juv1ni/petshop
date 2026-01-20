195b705867cea1d2d74c59142381da24c9d1fe78
Juv1ni <jorgegustavo45@gmail.com>
2026-01-20 09:20:46 -0300

    criando site de petphoto, feito até o momento o header e hero está sendo feito

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,14 @@
+export function Hero() {
+  return (
+    <section className="bg-pink-400 py-30">
+      <div className="container mx-auto px-4">
+        <h2 className="text-4xl font-bold text-white mb-4">
+          Bem-vindo ao Site
+        </h2>
+        <p className="text-lg text-white">
+          Aqui você encontrará informações sobre a Luz, Camera, Pata
+        </p>
+      </div>
+    </section>
+  );
+}
